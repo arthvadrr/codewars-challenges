35c85bbf5e1aa081a3b4793b3b8fab5fe7570114
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2022-10-24 23:36:51 -0500

    solving poker ranking

--- 4-kyu/ranking-poker-hands.js
+++ 4-kyu/ranking-poker-hands.js
@@ -1,4 +1,4 @@
-/*
+ /*
 https://www.codewars.com/kata/5739174624fc28e188000465/train/javascript
 
 A famous casino is suddenly faced with a sharp decline of their revenues. They decide to offer Texas hold'em also online. Can you help them by writing an algorithm that can rank poker hands?
@@ -41,10 +41,80 @@ If you finished this kata, you might want to continue with Sortable Poker Hands
 const Result = { "win": 1, "loss": 2, "tie": 3 }
 
 const PokerHand = hand => {
-  const cardValues = {}
+  hand = hand.split(' ');
 
+  const handValues = hand.map(item => {
+    item = item.split('');
+    switch(item[0]) {
+        case 'T': item[0] = 10; break;
+        case 'J': item[0] = 11; break;
+        case 'Q': item[0] = 12; break;
+        case 'K': item[0] = 13; break;
+        case 'A': item[0] = 14; break;
+        default: item[0] = parseInt(item[0]);
+    }
+    return item;
+  })
+
+  hand = handValues;
+  const cardNumbers = hand.map(i => i[0]).sort((a, b) => a - b);
+  const cardSuits = hand.map(i => i[1]).sort();
+  const highCard = cardNumbers[cardNumbers.length - 1];
+
+  console.log(cardNumbers);
+
+  const isStraight = () => {
+    for (let i = 1; i < cardNumbers.length; i++) {
+      if (cardNumbers[i - 1] + 1 !== cardNumbers[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  const isFlush = () => cardSuits.every(a => a === cardSuits[0]);
+
+  const isRoyalFlush = () => {
+    if (isStraight() && isFlush() && highCard === 14) {
+        return true;
+    }
+  }
+
+  const isStraightFlush = () => {
+    if (isStraight() && isFlush() && highCard !== 14) {
+        return true;
+    }
+  }
+
+  const findMatches = () => {
+    const matches = [];
+    for (let i = 0; i < 14; i++) {
+      let filter = cardNumbers.filter(num => num === i);
+      if (filter.length > 1) {
+        matches.push(filter);
+      } 
+    }
+    return matches;
+  }
+
+  const matches = findMatches();
+  const isFullHouse = () => matches[0].length + matches[1].length === 5;
+  const isTwoPair = () => matches[0].length + matches[1].length === 4;
+  const isFourKind = () => matches[0].length === 4;
+  const isThreeKind = () => matches[0].length === 3 && matches.length === 1;
+  const isPair = () => matches[0].length === 2 && matches.length === 1;
+
+  console.log(isFullHouse());
+  console.log(isTwoPair());
+  console.log(isFourKind());
+  console.log(isThreeKind());
+  console.log(isPair());
+
+  const isHighCard = () => {}
 }
 
-PokerHand.prototype.compareWith = function(hand){
-    return Result.tie;
-}
+PokerHand('KS KH KC JD JD');
+
+// PokerHand.prototype.compareWith = function(hand){
+//     return Result.tie;
+// }
