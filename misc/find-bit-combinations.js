a8f06abb3cc6a6d32f85bab75a3d8cb4acc25450
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2022-10-28 13:55:30 -0500

    renamed bit-pos

--- misc/find-bit-combinations.js
+++ misc/find-bit-combinations.js
@@ -1,6 +1,4 @@
-
 const allBitCombinations = size => {
-
   //edge cases
   if (size === 0) return;
   if (size === 1) return ['0', '1'];
@@ -9,29 +7,30 @@ const allBitCombinations = size => {
   const bit = Array.from({length: size}).fill(0)
   const flip = (arr, b) => arr[b] = arr[b] === 0 ? 1 : 0;
   const amount = Math.pow(2, size);
-  let p = size - 1;
+  let lastIndex = size - 1;
 
   const multMod = (i) => {
     let a = 1;
     for (let m = 2; m < amount; m = m*2) {
-      if (i % m === 0) flip(bit, p - a)
+      if (i % m === 0) flip(bit, lastIndex - a)
       a++;
     }
   }
 
   for (let i = 0; i < amount; i++) {
-    if (i % 2 === 0) flip(bit, p);
-    else flip(bit, p - 1)
-
+    // flip the last two every other time
+    if (i % 2 === 0) flip(bit, lastIndex);
+    else flip(bit, lastIndex - 1)
     multMod(i);
     let r = bit.join('');
     arr.push(r);
   }
 
-  console.log(arr);
+  console.table(arr);
   return arr;
 }
 
+//careful what you pass here
 allBitCombinations(3);
 
 /*
