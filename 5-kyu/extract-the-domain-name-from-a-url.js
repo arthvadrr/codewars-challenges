37444d6113cef70b0ddea9b6922a1e126d87a140
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2022-12-08 13:38:04 -0600

    added test cases against regex patterns

--- 5-kyu/extract-the-domain-name-from-a-url.js
+++ 5-kyu/extract-the-domain-name-from-a-url.js
@@ -12,6 +12,8 @@ const domainName = url => {
   const curl = url.split('')
   
   for (const l in url) {
+    if (l > 8) break;
+    
     if (
       url[l - 2] === '/' &&
       url[l - 1] === '/'
@@ -22,6 +24,8 @@ const domainName = url => {
   }
   
   for (const l in curl) {
+    if (l > 4) break;
+    
     if (
       curl[l - 1] === '.' && 
       curl[l - 2] === "w" && 
@@ -42,3 +46,37 @@ const domainName = url => {
   
   return curl.join('')
 }
+
+function domainName2(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
+  return url.split('.')[0];
+};
+
+function domainName3(url){
+  return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+}
+
+console.time("mine");
+console.log(domainName("http://google.com"));
+console.log(domainName("http://google.co.jp"));
+console.log(domainName("www.xakep.ru"));
+console.log(domainName("https://youtube.com"));
+console.timeEnd("mine");
+
+
+console.time("bestSolution1");
+console.log(domainName2("http://google.com"));
+console.log(domainName2("http://google.co.jp"));
+console.log(domainName2("www.xakep.ru"));
+console.log(domainName2("https://youtube.com"));
+console.timeEnd("bestSolution1");
+
+
+console.time("bestSolution2");
+console.log(domainName3("http://google.com"));
+console.log(domainName3("http://google.co.jp"));
+console.log(domainName3("www.xakep.ru"));
+console.log(domainName3("https://youtube.com"));
+console.timeEnd("bestSolution2");
