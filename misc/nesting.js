3867cac904e193d84887e69cd3d87208e6590686
Bryan Patrick <54081465+javabryan@users.noreply.github.com>
2022-12-15 23:50:07 -0600

    misc testing and max-subarray

---
+++ misc/nesting.js
@@ -0,0 +1,27 @@
+// Example structure
+
+const arthsHardDrive = {
+  name: "/",
+  files: {},
+  dirs: {
+    "Death Star Plans": {
+      files: {},
+      dirs: {},
+      size: 29310
+    },
+    "Totally not porn": {
+      files: {},
+      dirs: {},
+      size: 108401928401982012089984109217480988977
+    },
+    "Cat Pictures": {
+      dirs: {},
+      size: 1231
+    },
+    "Cat Pictures": {
+      files: {},
+      dirs: {},
+      size: 4112532
+    },
+  },
+}
