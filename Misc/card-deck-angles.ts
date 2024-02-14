31a3ee933bd213d0a78be8e7605fec3169f63816
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-02-14 01:57:57 -0600

    Renaming

--- Misc/card-deck-angles.ts
+++ Misc/card-deck-angles.ts
@@ -1,21 +1,25 @@
 /**
  * A function for getting an evenly curved angle set of given amount. Supports even and odd amounts
  *
- * Angle offset is the amount of degrees to offset by
+ * Angle offset is the amount of degrees to offset by.
  *
  * e.g.  I have 10 elements that need to be rotated evenly, starting at 5 degrees and increasing by 5 degrees for each element.
- *       getAngles(amount = 10, angleOffset = 5, start = 5)
- *       // returns [-25, -20, -15, -10, -5, 5,  10,  15,  20, 25]
+ *       getAngles(amount = 4, angleOffset = 2, start = 2)
+ *       // returns [ -4, -2, 2, 4 ]
+ * Note that <startingAngle> is the first angled set, and not the center starting point for odd amounts.
+ *
+ * Demonstrated at https://codepen.io/javabryan/pen/oNVJgaK
  */
 interface RotationOptions {
   amount: number;
   angleOffset?: number;
-  start?: number;
+  startingAngle?: number;
+  max?: number;
 }
 
-function getAngles({ amount, angleOffset = 2, start = 2 }: RotationOptions): number[] {
+function getAngles({ amount, angleOffset = 2, startingAngle = 2, max = 45 }: RotationOptions): number[] {
   const result: number[] = [];
-  let currentAngle = start;
+  let currentAngle = startingAngle;
   let index = 0;
 
   if (amount % 2 !== 0) result.push(index++);
@@ -23,6 +27,8 @@ function getAngles({ amount, angleOffset = 2, start = 2 }: RotationOptions): num
   for (let i = index; i < amount; i += 2) {
     result.push(currentAngle, currentAngle * -1);
     currentAngle += angleOffset;
+
+    if (currentAngle > max) currentAngle = max;
   }
 
   return result.sort((a, b) => a - b);
@@ -42,14 +48,14 @@ interface FakeElements {
 }
 
 // Create our sample elements
-const elements: FakeElements[] = Array.from({ length: 10 }, () => ({
+const elements: FakeElements[] = Array.from({ length: 4 }, () => ({
   element: 'div',
   className: 'card',
   style: {
     transform: '',
   },
 }));
-const elementAngles = getAngles({ amount: elements.length, angleOffset: 5, start: 5 });
+const elementAngles = getAngles({ amount: elements.length, angleOffset: 2, startingAngle: 2 });
 
 // Apply the transform
 elementAngles.forEach((_, i) => {
